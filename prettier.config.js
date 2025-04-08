2672df74f8f5623100f2b382b45b1994d096fbc0
samyar <samyarkd@gmail.com>
2025-04-08 22:04:09 +0330

    init 📯📯📯📯

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^~/(.*)$",
+    "^[./]",
+  ],
+};
