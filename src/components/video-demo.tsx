2adfd1cc5dd9605ca4b1bce4c998de9f027ae4ca
samyar <samyarkd@gmail.com>
2025-04-18 01:07:10 +0330

    fix: better embed

--- src/components/video-demo.tsx
+++ src/components/video-demo.tsx
@@ -3,10 +3,14 @@ export default function VideoDemo() {
     <div className="pb-1">
       <div className="border-primary overflow-hidden rounded-lg border">
         <iframe
-          className="min-h-42 w-full"
-          src="https://youtube.com/embed/0peGuCt7UvA"
+          className="min-h-40 w-full"
+          src="https://www.youtube-nocookie.com/embed/0peGuCt7UvA?si=YXQiOCW2wRU57yYH"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
-        />
+        ></iframe>
       </div>
       <p className="text-muted-foreground text-center text-xs">Guide Video</p>
     </div>
